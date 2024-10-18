775e50711af24cd4611f2d65ea87dac702d76147
Sumit Nair <sumit.nair26@gmail.com>
2024-10-18 19:21:00 +0530

    Fixed issue with express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,51 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import jwt, {JWTPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import path from "path";
 
-const JWT_SECRET = "SomeSecret";
+const JWT_SECRET = "test123";
 
-const app = express;
-
-app.use=(cookieParser());
-app.use=(express.json());
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
 app.use(cors({
-    credentials:true,
-    origin:"http://localhost:5173"
+    credentials: true,
+    origin: "http://localhost:5173"
 }));
 
-app.post("/signin", (req:any, res:any)=>{
+app.post("/signin", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-
+    // do db validations, fetch id of user from db
     const token = jwt.sign({
-        id:1
+        id: 1
     }, JWT_SECRET);
-    res.cookie("token", token)
-    res.send("Logged in");
-})
+    res.cookie("token", token);
+    res.send("Logged in!");
+});
 
-app.get("/user", (req:any, res:any)=>{
+app.get("/user", (req, res) => {
     const token = req.cookies.token;
-    const decoded = jwt.verify(token, JWT_SECRET) as JWTPayload;
-    //Get the user details from the database 
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    // Get email of the user from the database
     res.send({
         userId: decoded.id
     })
-})
+});
 
-app.post("logout", (req:any, res:any)=>{
+
+app.post("/logout", (req, res) => {
     res.clearCookie("token");
     res.json({
-        message:"Logged Out"
+        message: "Logged out!"
     })
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../src/index.html"))
 })
 
-app.listen(3000)
+const appListin = app.listen(3000);
+console.log("app started at", appListin);
